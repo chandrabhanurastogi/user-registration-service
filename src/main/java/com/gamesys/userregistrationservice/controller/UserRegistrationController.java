0b00cf07237c792640aa3f3aec5aa904f02f1348
pnegi-centrica <preeti.negi@centrica.com>
2019-10-16 09:04:54 +0100

    added todo

--- src/main/java/com/gamesys/userregistrationservice/controller/UserRegistrationController.java
+++ src/main/java/com/gamesys/userregistrationservice/controller/UserRegistrationController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
 public class UserRegistrationController {
 
     @PostMapping("/register")
     public ResponseEntity registerUser(@RequestBody User user){
         System.out.println(user.toString());
+        //todo validation
         return ResponseEntity.status(HttpStatus.OK).body("Registration Success");
     }
 }
